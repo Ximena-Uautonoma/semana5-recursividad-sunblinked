207cc435a58b4e9b9cf5efd43924fe80d1bba2c2
diego c <dcamposvargas05@gmail.com>
2026-04-02 14:34:50 +0000

    ej3

--- Semana5/ejercicio3.py
+++ Semana5/ejercicio3.py
@@ -6,8 +6,21 @@ Debe implementar una versión iterativa y una recursiva.
 """
 
 def factorial_ciclo(n):
-    pass
+    fac = 1
+    if n == 0:
+        return 0
+    for i in range(1, n + 1):
+        fac *= i
+    return fac
 
 
 def factorial_recursivo(n):
-    pass
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    return n * factorial_recursivo(n - 1)
+    
+x = int(input("ingrese num: "))
+print(f"factorial ciclo: {factorial_ciclo(x)}")
+print(f"factorial recursivo: {factorial_recursivo(x)}")
